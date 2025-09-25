189e9911d21bc5e5a2ee617aac14f564bcc71344
NjilaBoris <njilaboris2@gmail.com>
2025-09-25 20:01:25 +0100

    Implement getUserCompanions Server Action

--- app/my-journey/page.tsx
+++ app/my-journey/page.tsx
@@ -1,5 +1,15 @@
-const Profile = () => {
-  return <div>Profilepage</div>;
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const Profile = async () => {
+  const user = await currentUser();
+
+  if (!user) redirect("/sign-in");
+  return (
+    <main className="min-lg:w-3/4">
+      <section className="flex justify-between gap-4 max-sm:flex-col items-center"></section>
+    </main>
+  );
 };
 
 export default Profile;
--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,31 @@ import CompanionCard from "@/components/CompanionCard";
 import CompanionList from "@/components/CompanionList";
 import CTA from "@/components/CTA";
 import { recentSessions } from "@/constants";
+import {
+  getAllCompanions,
+  getRecentSessions,
+} from "@/lib/actions/companion.actions";
+import { getSubjectColor } from "@/lib/utils";
 
-const Home = () => {
+const Home = async () => {
+  const companions = await getAllCompanions({ limit: 3 });
+  const recentSessionsCompanions = await getRecentSessions(10);
   return (
     <main>
       <h1>Popular Companies</h1>
       <section className="home-section">
-        <CompanionCard />
-        <CompanionCard />
-        <CompanionCard />
+        {companions.map((companion) => (
+          <CompanionCard
+            key={companion.id}
+            {...companion}
+            color={getSubjectColor(companion.subject)}
+          />
+        ))}
       </section>
       <section className="home-section">
         <CompanionList
           title="Recently completed sessions"
-          companions={recentSessions}
+          companions={recentSessionsCompanions}
           classNames="w-2/3 max-lg:w-full"
         />
         <CTA />
