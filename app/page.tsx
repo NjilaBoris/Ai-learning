095a360d5b95d8c81cf81a254a45547e1dc68131
NjilaBoris <njilaboris2@gmail.com>
2025-09-26 01:23:14 +0100

    V1

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,8 @@ import {
 } from "@/lib/actions/companion.actions";
 import { getSubjectColor } from "@/lib/utils";
 
+export const dynamic = "force-dynamic";
+
 const Home = async () => {
   const companions = await getAllCompanions({ limit: 3 });
   const recentSessionsCompanions = await getRecentSessions(10);
